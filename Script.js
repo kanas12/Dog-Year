33cc1062d7bce2b556750fa1f0f0c47b19130939
Daniel Frimpong <daniel.frimpong@amlitech.org>
2021-08-18 13:56:10 +0000

    first commit

---
+++ Script.js
@@ -0,0 +1,18 @@
+//current age
+const myAge = 15;
+//early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+// change earlyyears to lateryears
+let laterYears = myAge -2;
+// multiply the laterYears variable by 4
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(earlyYears);
+// add
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Kwabena'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge}.`);
